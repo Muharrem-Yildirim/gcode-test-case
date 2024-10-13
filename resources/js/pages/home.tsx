a311ef9d9471ffffade11ef317b066b695a14722
Muharrem Yıldırım <muharremyildirim303@gmail.com>
2024-10-13 23:08:01 +0300

    Update home.tsx

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,10 +1,29 @@
+import { CurrenciesDatatable } from "@/components/currencies-datatable";
 import { Head } from "@inertiajs/react";
+import { TableProvider } from "@/context/table-context";
+import { CurrencyQueryProvider } from "@/context/currency-query-context";
+import Header from "@/components/pages/main/header";
+import { Toaster } from "@/components/ui/toaster";
 
 export default function Home() {
     return (
         <>
             <Head title="Home" />
-            Hello world!
+            <Toaster />
+            <Main />
         </>
     );
 }
+
+const Main = () => {
+    return (
+        <CurrencyQueryProvider>
+            <TableProvider>
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 mt-10 px-4">
+                    <Header />
+                    <CurrenciesDatatable />
+                </div>
+            </TableProvider>
+        </CurrencyQueryProvider>
+    );
+};
