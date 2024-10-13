f7ecdff46605968e888194db8bda1b3245849fff
Muharrem Yıldırım <muharremyildirim303@gmail.com>
2024-10-13 13:01:27 +0300

    CACHED type added to enum

--- app/Enums/TCMBDebugMessageTypesEnum.php
+++ app/Enums/TCMBDebugMessageTypesEnum.php
@@ -8,6 +8,7 @@ namespace App\Enums;
 enum TCMBDebugMessageTypesEnum: string
 {
     case UNKNOWN = "UNKNOWN";
+    case CACHED = "CACHED";
     case WEEKEND = "WEEKEND";
     case AFTER_TODAY = "AFTER_TODAY";
 }
