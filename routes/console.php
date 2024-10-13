6b62d3508c76355bdbe2f4a6a6aa7faf54da2324
Muharrem Yıldırım <muharremyildirim303@gmail.com>
2024-10-13 13:04:14 +0300

    import-today command added to scheduler

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Services\TCMBImporter;
 use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('app:import-today', function () {
+    $this->info("Importing TCMB data for today");
+})->purpose('Import Today')->daily();
